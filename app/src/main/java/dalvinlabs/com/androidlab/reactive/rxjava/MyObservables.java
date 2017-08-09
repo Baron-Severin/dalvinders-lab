b55f76253901e20d37609399d3bad5bb86328a47
Dalvinder Singh <singh.dalvin@gmail.com>
2017-08-09 08:36:39 -0700

    RX

--- app/src/main/java/dalvinlabs/com/androidlab/reactive/rxjava/MyObservables.java
+++ app/src/main/java/dalvinlabs/com/androidlab/reactive/rxjava/MyObservables.java
@@ -2,15 +2,19 @@ package dalvinlabs.com.androidlab.reactive.rxjava;
 
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class MyObservables {
@@ -19,16 +23,48 @@ public class MyObservables {
 
     private static CompositeDisposable compositeDisposable = new CompositeDisposable();
 
+    private static class MyObserver<T> implements Observer<T> {
+
+        MyObserver() {
+
+        }
+
+        @Override
+        public void onSubscribe(Disposable d) {
+            System.out.println("MyObserver :: onSubscribe");
+        }
+
+        @Override
+        public void onNext(T value) {
+            System.out.println("MyObserver :: onNext");
+            System.out.println("thread = " + Thread.currentThread().getName());
+            System.out.println("output = " + value);
+            System.out.println("-----");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            System.out.println("MyObserver :: onError = " + e);
+            System.out.println("-----");
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println("MyObserver :: onComplete");
+            System.out.println("-----");
+        }
+    }
+
     private static Observer<String> genericObserver1 = new Observer<String>() {
         @Override
         public void onSubscribe(Disposable d) {
             System.out.println("genericObserver1 :: onSubscribe");
             compositeDisposable.add(d);
+            System.out.println("-----");
         }
 
         @Override
         public void onNext(String output) {
-            System.out.println("-----");
             System.out.println("genericObserver1 :: onNext");
             System.out.println("thread = " + Thread.currentThread().getName());
             System.out.println("output = " + output);
@@ -38,7 +74,7 @@ public class MyObservables {
         @Override
         public void onError(Throwable e) {
             System.out.println("genericObserver1 :: onError = " + e);
-            e.printStackTrace();
+            //e.printStackTrace();
             System.out.println("-----");
         }
 
@@ -54,11 +90,11 @@ public class MyObservables {
         public void onSubscribe(Disposable d) {
             System.out.println("genericObserver2 :: onSubscribe");
             compositeDisposable.add(d);
+            System.out.println("-----");
         }
 
         @Override
         public void onNext(String output) {
-            System.out.println("-----");
             System.out.println("genericObserver2 :: onNext");
             System.out.println("thread = " + Thread.currentThread().getName());
             System.out.println("output = " + output);
@@ -68,15 +104,16 @@ public class MyObservables {
         @Override
         public void onError(Throwable e) {
             System.out.println("genericObserver2 :: onError = " + e.getMessage());
+            System.out.println("-----");
         }
 
         @Override
         public void onComplete() {
             System.out.println("genericObserver2 :: onComplete");
+            System.out.println("-----");
         }
     };
 
-    // # # # # # CREATE
     private static Consumer<List<String>> consumerList = data ->
             System.out.println("consumerList :: accept = " + data.toString());
 
@@ -108,9 +145,9 @@ public class MyObservables {
      */
     public static void fromCallable() {
         Observable<List<String>> myObservable = Observable.fromCallable(() -> {
-                    System.out.println("call, thread = " + Thread.currentThread().getName());
-                    return Utils.getData();
-                });
+            System.out.println("call, thread = " + Thread.currentThread().getName());
+            return Utils.getData();
+        });
         myObservable.subscribeOn(Schedulers.io())
                 //.observeOn(AndroidSchedulers.mainThread()) For ANDROID use Android Schedulers.
                 .observeOn(Schedulers.newThread()) // For ease of unit test, this is used
@@ -133,16 +170,24 @@ public class MyObservables {
         myObservable.subscribe(genericObserver1);
     }
 
+
+
     /*
         1. Create custom observable, code is responsible for calling onNext, onComplete|onError.
         2. Observable is NOT created until subscription similar to "defer"
+        3. create can emit multiple values.
      */
     public static void create() {
         Observable<String> myObservable = Observable.create((emitter) -> {
-            if (!emitter.isDisposed()) {
-                System.out.println("got emitter = " + emitter);
-                emitter.onNext("from create");
-                emitter.onError(new Throwable("Just a test"));
+            try {
+                if (!emitter.isDisposed()) {
+                    System.out.println("got emitter = " + emitter);
+                    emitter.onNext("from create - 1");
+                    emitter.onNext("from create - 2");
+                    emitter.onComplete();
+                }
+            } catch (Exception e) {
+                emitter.onError(e);
             }
         });
         myObservable.subscribe(genericObserver1);
@@ -181,21 +226,53 @@ public class MyObservables {
     }
 
     /*
-        Generate observable
-        TODO: more understanding required
-     */
-    public static void generate() {
-        Observable<String> myObservable = Observable.generate( (emitter -> {
+        1. Similar to create
+        2. Stateless
+    */
+    public static void generateStateless() {
+        Observable<String> myObservable = Observable.generate((emitter -> {
             System.out.println("got emitter = " + emitter);
-            emitter.onNext("from generate");
-            emitter.onError(new Throwable("Just a test"));
+            try {
+                emitter.onNext("generate - 1");
+                emitter.onNext("generate - 2 ");
+                emitter.onComplete();
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
         }));
         myObservable.subscribe(genericObserver1);
-        System.out.println("-----");
-        myObservable.subscribe(genericObserver2);
     }
 
-
+    /*
+        1. Stateful generate
+        2. Gets initial state
+        3. Generate data and return new state
+        4. After terminal operation consumer with dispose state gets called.
+     */
+    public static void generateStateful() {
+        Observable<String> statefulObservable = Observable.generate(
+                () -> {
+                    int initialState = 1;
+                    System.out.println("initial state = " + initialState);
+                    return initialState;
+                },
+                (initialState, stringEmitter) -> {
+                    try {
+                        if (initialState <= 3) {
+                            System.out.println("emitter = " + stringEmitter);
+                            stringEmitter.onNext("generate - " + initialState);
+                            initialState += 1;
+                            System.out.println("update state = " + initialState);
+                        } else {
+                            stringEmitter.onComplete();
+                        }
+                    } catch (Exception e) {
+                        stringEmitter.onError(e);
+                    }
+                    return initialState;
+                }, (disposeState -> System.out.println("dispose state = " + disposeState)));
+        statefulObservable.subscribe(genericObserver1);
+    }
 
     /*
         Stack trace test
@@ -210,5 +287,85 @@ public class MyObservables {
         myObservable.subscribe(item -> System.out.println("output = " + item));
     }
 
+    /*
+        Return nothing but completes
+     */
+    public static void empty() {
+        Observable<String> myObservable = Observable.empty();
+        myObservable.subscribe(genericObserver1);
+    }
+
+    /*
+        Neither return nor completes
+     */
+    public static void never() {
+        Observable<String> myObservable = Observable.never();
+        myObservable.subscribe(genericObserver1);
+    }
+
+    /*
+        Return nothing but terminates with error.
+     */
+    public static void error() {
+        Observable<String> myObservable = Observable.error(
+                () -> new Exception("Exception from error observable"));
+        myObservable.subscribe(genericObserver1);
+    }
+
+    /*
+        By default runs on computation thread.
+     */
+    public static void interval(final CountDownLatch latch) {
+        Observable<Long> myObservable = Observable.interval(0l, 30l,
+                TimeUnit.MILLISECONDS);
+        myObservable.subscribe(item -> System.out.println("output = " + item),
+                throwable -> {
+                    System.out.println("error = " + throwable);
+                    latch.countDown();
+                },
+                () -> {
+                    System.out.println("onComplete");
+                    latch.countDown();
+                });
+    }
+
+    /*
+        By default runs on computation thread.
+     */
+    public static void intervalRange() {
+        Observable.intervalRange(0l, 10l, 0l, 10l, TimeUnit.MILLISECONDS).
+                subscribe(new MyObserver<>());
+    }
+
+    public static void range() {
+        Observable.range(0, 10).subscribe(new MyObserver<>());
+    }
+
+    public static void repeat() {
+        Observable.just("abc").repeat(3).subscribe(new MyObserver<>());
+    }
+
+    public static void repeatUntil() {
+
+        class Data {
+            private int value = 0;
+        }
+
+        final Data data = new Data();
+
+        Observable.just("abc").repeatUntil(() -> {
+            data.value += 1;
+            return (data.value == 5);
+        }).subscribe(new MyObserver<>());
+    }
+
+    public static void repeatWhen() {
+        Observable.just("abc").repeatWhen(new Function<Observable<Object>, ObservableSource<?>>() {
+            @Override
+            public ObservableSource<?> apply(Observable<Object> objectObservable) throws Exception {
+                return null;
+            }
+        });
+    }
 
 }
--- app/src/test/java/dalvinlabs/com/androidlab/MyObservablesTest.java
+++ app/src/test/java/dalvinlabs/com/androidlab/MyObservablesTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import dalvinlabs.com.androidlab.reactive.rxjava.Hybrids;
 import dalvinlabs.com.androidlab.reactive.rxjava.MyObservables;
@@ -15,47 +17,97 @@ import dalvinlabs.com.androidlab.reactive.rxjava.MySingle;
 public class MyObservablesTest {
 
     @Test
-    public void testObservableJust() throws Exception {
+    public void testJust() throws Exception {
         MyObservables.just();
     }
 
     @Test
-    public void testObservableFromArray() throws Exception {
+    public void testFromArray() throws Exception {
         MyObservables.fromArray();
     }
 
     @Test
-    public void testObservableFromCallable() throws Exception {
+    public void testFromCallable() throws Exception {
         MyObservables.fromCallable();
     }
 
     @Test
-    public void testObservableFromIterable() throws Exception {
+    public void testFromIterable() throws Exception {
         MyObservables.fromIterable();
     }
 
     @Test
-    public void testObservableFromFuture() throws Exception {
+    public void testFromFuture() throws Exception {
         MyObservables.fromFuture();
     }
 
     @Test
-    public void testObservableCreate() throws Exception {
+    public void testCreate() throws Exception {
         MyObservables.create();
     }
 
     @Test
-    public void testObservableGenerate() throws Exception {
-        MyObservables.generate();
+    public void testGenerateStateless() throws Exception {
+        MyObservables.generateStateless();
     }
 
     @Test
-    public void testObservableDefer() throws Exception {
+    public void testGenerateStateful() throws Exception {
+        MyObservables.generateStateful();
+    }
+
+    @Test
+    public void testDefer() throws Exception {
         MyObservables.defer();
     }
 
     @Test
-    public void testObservableStackTrace() throws Exception {
+    public void testStackTrace() throws Exception {
         MyObservables.stackTrace();
     }
+
+    @Test
+    public void testEmpty() throws Exception {
+        MyObservables.empty();
+    }
+
+    @Test
+    public void testNever() throws Exception {
+        MyObservables.never();
+    }
+
+    @Test
+    public void testError() throws Exception {
+        MyObservables.error();
+    }
+
+    @Test
+    public void testInterval() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        MyObservables.interval(latch);
+        latch.await(10l, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testIntervalRange() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        MyObservables.intervalRange();
+        latch.await(10l, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testRange() throws Exception {
+        MyObservables.range();
+    }
+
+    @Test
+    public void testRepeat() throws Exception {
+        MyObservables.repeat();
+    }
+
+    @Test
+    public void testRepeatUntil() throws Exception {
+        MyObservables.repeatUntil();
+    }
+
 }
