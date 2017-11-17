1332555c068e0cd20b29fe3f1f32bea8d4310c85
Dalvinder Singh <singh.dalvin@gmail.com>
2017-11-17 00:02:07 -0800

    selection sort

---
+++ app/src/test/java/dalvinlabs/com/androidlab/algo/sort/selection/SelectionSortTest.java
@@ -0,0 +1,22 @@
+package dalvinlabs.com.androidlab.algo.sort.selection;
+
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SelectionSortTest {
+
+    @Test
+    public void selectionSort() {
+        char[] input = {'D', 'B', 'C', 'A', 'F', 'G', 'E', 'I', 'H', 'K', 'J', 'L', 'M'};
+        char[] expected = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+        char[] output = SelectionSort.sort(input);
+        if (expected.length != output.length) {
+            Assert.fail("Expected and Output length mismatch");
+        }
+        Assert.assertTrue("Output is not as expected", Arrays.equals(output, expected));
+    }
+}
